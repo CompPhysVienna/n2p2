2269bf7bdc81eedd92bb88c35adaa08acfa5de6b
Andreas Singraber <andreas.singraber@gmx.at>
2018-11-23 23:03:29 +0100

    Started to clean up examples.

--- src/libnnp/version.h
+++ src/libnnp/version.h
@@ -18,8 +18,8 @@
 #define VERSION_H
 
 #define NNP_VERSION "2.0.0"
-#define NNP_GIT_REV "7b423664b02ff4e4979301b4a136ac3221f46be2"
-#define NNP_GIT_REV_SHORT "7b42366"
+#define NNP_GIT_REV "ae550f21c7763a38f465c249a2cab54a419fd2a3"
+#define NNP_GIT_REV_SHORT "ae550f2"
 #define NNP_GIT_BRANCH "master"
 
 #endif
