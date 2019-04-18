e0e8c2c4181874574eba6a7b497c8f40651c77c9
Florian Buchner <florian.buchner@tuwien.ac.at>
2019-04-18 11:09:57 +0200

    Began reworking storage of symfunc params.
    r_shift, eta, zeta now stored in individual arrays, information on
    radial parameter generation settings stored in dictionary.

--- tools/python/SymFuncParamGenerator.py
+++ tools/python/SymFuncParamGenerator.py
@@ -10,43 +10,43 @@ class SymFuncParamGenerator:
     def __init__(self, elements):
         self.elements = elements
 
-        self.sf_list_radial = []
-        self.sf_list_ang_narrow = []
-        self.sf_list_ang_wide = []
+        self.r_shift_grid = None
+        self.eta_grid = None
 
-        self.generation_info_radial = ''
-        self.generation_info_ang_narrow = ''
-        self.generation_info_ang_wide = ''
+        self.r_cutoff = None
+        self.symfunc_type = None
+        self.radial_grid_info = None
+
+        self.lambdas = None
+        self.zetas = [-1.0, 1.0]
 
     def get_elements(self):
         return self.elements
 
-    def clear_radial(self):
-        self.sf_list_radial = []
-        self.generation_info_radial = ''
+    def clear_all(self):
+        # TODO: make this do something...
+        pass
 
-    def clear_ang_narrow(self):
-        self.sf_list_ang_narrow = []
-        self.generation_info_ang_narrow = ''
+    def generate_radial_params(self, method, mode, r_cutoff, nb_gridpoints, r_lower=None):
+        # store infos on radial parameter generation settings (that are independent of method)
+        self.radial_grid_info = {'method': method, 'mode': mode, 'r_cutoff': r_cutoff, 'nb_gridpoints': nb_gridpoints}
 
-    def clear_ang_wide(self):
-        self.sf_list_ang_wide = []
-        self.generation_info_ang_wide = ''
-
-    def generate_radial_grid(self, method, mode, r_cutoff, nb_gridpoints, r_lower=None):
         if method == 'gastegger2018':
             if r_lower is None:
                 raise TypeError('Argument r_lower is required for method "gastegger2018"')
 
             r_upper = r_cutoff - 0.5  # TODO: decide if hardcoding this (and in Angstrom!) is a good idea
+            # store settings that are unique to this method
+            self.radial_grid_info.update({'r_lower': r_lower, 'r_upper': r_upper})
+
             # create auxiliary grid
             grid = np.linspace(r_lower, r_upper, nb_gridpoints)
 
             if mode == 'center':
-                rs_grid = np.zeros(nb_gridpoints)
+                r_shift_grid = np.zeros(nb_gridpoints)
                 eta_grid = 1.0 / (2.0 * grid ** 2)
             elif mode == 'shift':
-                rs_grid = grid
+                r_shift_grid = grid
                 dr = (r_upper - r_lower) / (nb_gridpoints - 1)
                 eta_grid = np.full(nb_gridpoints, 1.0 / (2.0 * dr * dr))
             else:
@@ -54,7 +54,7 @@ class SymFuncParamGenerator:
 
         elif method == 'imbalzano2018':
             if r_lower is not None:
-                # TODO: decide if it makes sense to do the warning this way
+                # TODO: decide if necessary to do this warning and if doing it this way makes sense
                 sys.stderr.write(
                     'Warning: argument r_lower is not used in method "imbalzano2018" and will be ignored.\n')
                 traceback.print_stack()
@@ -64,57 +64,70 @@ class SymFuncParamGenerator:
                 nb_intervals = nb_gridpoints - 1
                 gridpoint_indices = np.array(range(0, nb_intervals + 1))
                 eta_grid = (nb_intervals ** (gridpoint_indices / nb_intervals) / r_cutoff) ** 2
-                rs_grid = np.zeros_like(eta_grid)
+                r_shift_grid = np.zeros_like(eta_grid)
             elif mode == 'shift':
                 # create extended auxiliary grid of r_shift values, that contains nb_gridpoints + 1 values
                 nb_intervals_extended = nb_gridpoints
                 gridpoint_indices_extended = np.array(range(0, nb_intervals_extended + 1))
                 rs_grid_extended = r_cutoff / nb_intervals_extended ** (
                         gridpoint_indices_extended / nb_intervals_extended)
-                # from pairs of neighboring r_shift values, compute eta values
+                # from pairs of neighboring r_shift values, compute eta values.
                 # doing this for the nb_gridpoints + 1 values in the auxiliary grid ultimately gives...
                 # ...nb_gridpoints different values for eta
                 eta_grid = np.zeros(nb_gridpoints)
                 for idx in range(len(rs_grid_extended) - 1):
                     eta_current = 1 / (rs_grid_extended[idx] - rs_grid_extended[idx + 1]) ** 2
                     eta_grid[idx] = eta_current
-                # create final grid of rs_shift values by excluding the first entry...
+                # create final grid of r_shift values by excluding the first entry...
                 # ...(for which r_shift coincides with the cutoff radius) from the extended grid
-                rs_grid = rs_grid_extended[1:]
-                # reverse the order of r_shift and eta values so they are sorted in ascending order of r_shift...
+                r_shift_grid = rs_grid_extended[1:]
+                # reverse the order of r_shift and eta values so they are sorted in order of ascending r_shift...
                 # ...(not necessary, but makes the output consistent with the other methods)
-                rs_grid = np.flip(rs_grid)
+                r_shift_grid = np.flip(r_shift_grid)
                 eta_grid = np.flip(eta_grid)
             else:
                 raise ValueError('invalid argument for "mode"')
         else:
             raise ValueError('invalid argument for "method"')
 
-        return rs_grid, eta_grid
+        self.r_shift_grid = r_shift_grid
+        self.eta_grid = eta_grid
+
+    def set_zetas(self, zeta_values):
+        self.zetas = zeta_values
+
+    def set_symfunc_type(self, symfunc_type):
+        self.symfunc_type = symfunc_type
+
+    def write_generation_info(self):
+        pass
+
+    def write_parameter_strings(self):
+        pass
 
 
 elems = ['Cu', 'S']
-myGenerator = SymFuncParamGenerator(elems)
+myGen = SymFuncParamGenerator(elems)
 
-print(myGenerator.get_elements())
+print(myGen.get_elements())
 
 print('\ngastegger2018 center mode')
-a, b = myGenerator.generate_radial_grid(method='gastegger2018', mode='center', r_cutoff=6., nb_gridpoints=9,
-                                        r_lower=1.5)
-print(a)
-print(b)
+myGen.generate_radial_params(method='gastegger2018', mode='center', r_cutoff=6., nb_gridpoints=9,
+                             r_lower=1.5)
+print(myGen.r_shift_grid)
+print(myGen.eta_grid)
 
 print('\ngastegger2018 shift mode')
-a, b = myGenerator.generate_radial_grid(method='gastegger2018', mode='shift', r_cutoff=6., nb_gridpoints=9, r_lower=1.5)
-print(a)
-print(b)
+myGen.generate_radial_params(method='gastegger2018', mode='shift', r_cutoff=6., nb_gridpoints=9, r_lower=1.5)
+print(myGen.r_shift_grid)
+print(myGen.eta_grid)
 
 print('\nimbalzano2018 center mode')
-a, b = myGenerator.generate_radial_grid(method='imbalzano2018', mode='center', r_cutoff=6., nb_gridpoints=5)
-print(a)
-print(b)
+myGen.generate_radial_params(method='imbalzano2018', mode='center', r_cutoff=5., nb_gridpoints=5)
+print(myGen.r_shift_grid)
+print(myGen.eta_grid)
 
 print('\nimbalzano2018 shift mode')
-a, b = myGenerator.generate_radial_grid(method='imbalzano2018', mode='shift', r_cutoff=6., nb_gridpoints=5)
-print(a)
-print(b)
+myGen.generate_radial_params(method='imbalzano2018', mode='shift', r_cutoff=6., nb_gridpoints=5)
+print(myGen.r_shift_grid)
+print(myGen.eta_grid)
