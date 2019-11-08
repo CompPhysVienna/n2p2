5fe29c36df752e301c2efe95a1313fa1589a289f
Andreas Singraber <andreas.singraber@gmx.at>
2019-11-08 12:05:44 +0100

    Add badges in README, excluded tests from coverage

--- src/libnnp/version.h
+++ src/libnnp/version.h
@@ -18,8 +18,8 @@
 #define VERSION_H
 
 #define NNP_VERSION "2.0.0"
-#define NNP_GIT_REV "e6cd59814039f5d5375bf44928be7eb88edb4cba"
-#define NNP_GIT_REV_SHORT "e6cd598"
-#define NNP_GIT_BRANCH "symfunc_memory_reduction"
+#define NNP_GIT_REV "452bad650e6aae253fcb5e9a6e00da58b406012d"
+#define NNP_GIT_REV_SHORT "452bad6"
+#define NNP_GIT_BRANCH "ci_tests"
 
 #endif
