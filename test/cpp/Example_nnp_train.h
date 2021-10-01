ce7073fd7a2802be73766f825a2cc23c6d082cca
Andreas Singraber <andreas.singraber@gmx.at>
2021-10-01 15:03:35 +0200

    Fixed nnp-train H2O test

--- test/cpp/Example_nnp_train.h
+++ test/cpp/Example_nnp_train.h
@@ -31,10 +31,10 @@ void BoostDataContainer<Example_nnp_train>::setup()
     examples.push_back(Example_nnp_train("H2O_RPBE-D3"));
     e = &(examples.back());
     e->lastEpoch = 5;
-    e->rmseEnergyTrain = 1.15047291E-03;
-    e->rmseEnergyTest  = 1.94904242E-04;
-    e->rmseForcesTrain = 2.61575846E-02;
-    e->rmseForcesTest  = 2.17373661E-02;
+    e->rmseEnergyTrain = 5.19563489E-04;
+    e->rmseEnergyTest  = 4.19002577E-04;
+    e->rmseForcesTrain = 2.41271658E-02;
+    e->rmseForcesTest  = 1.59314285E-02;
 
     return;
 }
